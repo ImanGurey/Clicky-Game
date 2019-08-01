8d4570d3ec8e697d2f055221fb92ed4de56004e4
Iman Gure <imangu99@gmail.com>
2019-07-31 23:07:13 -0700

    images

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => <div className="footer text-center">Iman Gure</div>;
+
+export default Footer;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import meryl from "./images/meryl.png";
+import allan from "./images/allan.png";
+import  dennis from "./images/dennis.png";
+import rick from "./images/rick.png";
+import walt from "./images/walt.png";
+import katharine from "./images/katharine.png";
+import john from "./images/john.png";
+import leon from "./images/leon.png";
+import edith from "./images/edith.png";
+import alfred from "./images/alfred.png";
+import cedric from "./images/cedric.png";
+import joseph from "./images/joseph.png";
+
+const images = [
+    meryl,
+    allan,
+    dennis,
+    rick,
+    walt,
+    katharine,
+    john,
+    leon,
+    edith,
+    alfred,
+    cedric,
+    joseph
+];
+
+export default images;
